d447dd09badbe86e411330439dd81b187b1e8b4d
corposhare <workforcorposhare@gmail.com>
2023-06-10 09:32:16 +0530

    update on company list

--- src/companyform/CompanyList.jsx
+++ src/companyform/CompanyList.jsx
@@ -1,53 +1,109 @@
 import React, { useState, useEffect } from 'react';
 import './CompanyList.scss';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const CompanyList = () => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+    // Commented out the API call and provided sample data as text
+    // const [loading, setLoading] = useState(true);
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://3.145.60.152/api/v1/companies/');
-                const fetchedData = await response.json();
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch('http://3.145.60.152/api/v1/companies/');
+    //             const fetchedData = await response.json();
 
-                setData(fetchedData);
-                setLoading(false);
-            } catch (error) {
-                console.error('An error occurred while fetching the data.', error);
-                setLoading(false);
-            }
-        };
+    //             setData(fetchedData);
+    //             setLoading(false);
+    //         } catch (error) {
+    //             console.error('An error occurred while fetching the data.', error);
+    //             setLoading(false);
+    //         }
+    //     };
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
+
+    // Sample data as text
+    const sampleData = [
+        {
+            id: 1,
+            logo: 'https://loremflickr.com/320/320/company',
+            company_name: 'Sample Company 1',
+            description: 'Sample description for Company 1',
+            email: 'sample1@example.com',
+            location: 'Sample Location 1',
+            founder: 'Sample Founder 1',
+            employees: '100',
+            year: '2005',
+            valuation: '$1 billion',
+            services: ['Service 1', 'Service 2', 'Service 3'],
+            desc: 'Sample description for Company 1',
+        },
+        {
+            id: 2,
+            logo: 'https://loremflickr.com/320/320/company',
+            company_name: 'Sample Company 2',
+            description: 'Sample description for Company 2',
+            email: 'sample2@example.com',
+            location: 'Sample Location 2',
+            founder: 'Sample Founder 2',
+            employees: '200',
+            year: '2010',
+            valuation: '$500 million',
+            services: ['Service 4', 'Service 5'],
+            desc: 'Sample description for Company 2',
+        },
+    ];
 
     return (
-        <div className="company-list">
-            {loading ? (
-                <div className="loading-indicator">Loading...</div>
-            ) : (
+        <>
+            <Navbar />
+            <br></br>
+            <div className="company-list">
                 <div className="company-list__container">
-                    {
-                        data.map((company) => (
-                            <div className="company-card" key={company.id}>
-                                <div className="logo">
-                                    <img src={company.logo} alt="Company Logo" />
-                                </div>
-                                <div className="details">
+                    {sampleData.map((company) => (
+                        <div className="company-card" key={company.id}>
+
+                            <div className="company-card__details">
+                                <div className='logo-name'>
+                                    <div className="company-card__logo">
+                                        <img src={company.logo} alt="Company Logo" />
+                                    </div>
                                     <h2>{company.company_name}</h2>
-                                    <p>{company.description}</p>
-                                    <p>Services Offered: {company.services_offered}</p>
-                                    <p>Year Established: {company.year_established}</p>
-                                    <p>Number of Employees: {company.employees}</p>
-                                    <p>Valuation: {company.valuation}</p>
+                                </div>
+                                <p>{company.description}</p>
+                                <div className="company-card__info">
+                                    <div className="info-item">
+                                        <span>Email:</span> {company.email}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Location:</span> {company.location}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Founder:</span> {company.founder}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Employees:</span> {company.employees}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Year Established:</span> {company.year}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Valuation:</span> {company.valuation}
+                                    </div>
+                                    <div className="info-item">
+                                        <span>Services:</span> {company.services.join(', ')}
+                                    </div>
                                 </div>
                             </div>
-                        ))
-                    }
+                        </div>
+                    ))}
                 </div>
-            )}
-        </div>
+            </div>
+            <Footer />
+        </>
     );
 };
 
